6b9b830ecea740fa917356600cf6884dd1018b35
SedatCeyhan <sedat.sabancidx@gmail.com>
2021-11-09 00:40:21 +0000

    oush the entire project - public

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 bg-gray-100 px-32 py-14 border-t text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Housing App works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Housing App Plus</p>
+        <p>Housing App Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome housing app</p>
+        <p>Referrals accepted</p>
+        <p>Kemal Sedat Ceyhan</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Sedat</p>
+        <p>Presents</p>
+        <p>A Template</p>
+        <p>Housing App Project</p>
+        <p>Check it out</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Loved doing</p>
+        <p>This UI</p>
+        <p>Take Care!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "raw.githubusercontent.com"],
+  },
+  env: {
+    mapbox_key: "pk.eyJ1Ijoic2VkZGV2IiwiYSI6ImNrc2o2YWE3cDBndWEycG5jb3phNXRkOTYifQ.6L42P1YT_RWu0B2knz9NQQ",
+  },
+};
